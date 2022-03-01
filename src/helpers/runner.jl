7801f3621c881e4d2b8cc6117be362848ddadb40
Jamie Mair <jamie.mair@hotmail.co.uk>
2022-03-01 14:15:20 +0000

    Added some code to allow for running code with a variety of parallel methods

---
+++ src/helpers/runner.jl
@@ -0,0 +1,58 @@
+module Runner
+using Distributed
+using ProgressMeter
+using Base.Iterators
+
+@enum TaskExecutionMode SerialMode ThreadedMode DistributedMode
+
+function get_results_distributed(fn, iter; show_progress=false)
+    if show_progress
+        results = @showprogress pmap(iter) do x
+            fn(x...)
+        end
+        return results
+    else
+        results = pmap(iter) do x
+            fn(x...)
+        end
+        return results
+    end
+end
+function get_results_threaded(fn, iter; show_progress=false)
+    results = Array{Any}(undef, size(iter)...)
+    progress_bar = show_progress ? Progress(length(iter)) : nothing
+    Threads.@threads for (i, x) in iter
+        results[i] = fn(x...)
+        show_progress && next!(progress_bar)
+    end
+    return results
+end
+function get_results_serial(fn, iter; show_progress=false)
+    if show_progress
+        results = @showprogress map(iter) do x
+            fn(x...)
+        end
+        return results
+    else
+        results = map(iter) do x
+            fn(x...)
+        end
+        return results
+    end
+end
+
+function get_results(fn, iter, mode::TaskExecutionMode=SerialMode; show_progress=false)
+    if mode==SerialMode
+        return get_results_serial(fn, iter; show_progress)
+    elseif mode==ThreadedMode
+        return get_results_threaded(fn, iter; show_progress)
+    elseif mode==DistributedMode
+        return get_results_distributed(fn, iter; show_progress)
+    else
+        throw("TaskExecutionMode $mode is unimplemented")
+    end
+end
+
+export TaskExecutionMode, get_results, SerialMode, ThreadedMode, DistributedMode
+
+end
