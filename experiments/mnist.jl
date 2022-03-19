5f0d7d2be55af08bd1ccccc90fe22ed3fec153d6
Jamie Mair <jamie.mair@nottingham.ac.uk>
2022-03-19 18:06:08 +0000

    Added options for type of run for mnist

--- experiments/mnist.jl
+++ experiments/mnist.jl
@@ -7,6 +7,7 @@ using Statistics
 using Flux
 using CUDA
 using TPS
+using NNE
 using TPS.Convergence
 
 
@@ -18,10 +19,10 @@ function get_experiment_parameter_dictionaries()
     trajectory_lengths = [1, 2, 4, 8, 16]
     options = Dict{Symbol, Any}()
     options[:fraction_to_include] = 0.25
-    options[:warmup_steps] = 100
-    options[:polling_frequency] = 10
-    options[:max_buffer_size] = 10000
-    options[:max_epochs] = 1000
+    options[:warmup_steps] = 75000
+    options[:polling_frequency] = 100
+    options[:max_buffer_size] = 400000
+    options[:max_epochs] = 500000
     options[:relative_gradient_size] = 1e-7
     options[:relative_error_size] = 1e-3
     options[:device] = gpu
@@ -37,25 +38,51 @@ function get_experiment_parameter_dictionaries()
     return input_dictionaries
 end
 
-function mnist_trajectory_experiment(; use_local_execution=false)
+function mnist_trajectory_experiment(; execution_mode=:serial)
     input_dictionaries = get_experiment_parameter_dictionaries()
 
-    
-    results = @showprogress pmap(input_dictionaries; distributed=use_local_execution) do dict
-        map_params_to_trajectory(dict)
+    # Threaded
+    if execution_mode==:threaded
+        results = convert(Matrix{Any}, similar(input_dictionaries))
+        progress = Progress(length(results))
+        Threads.@threads for i in 1:length(results)
+            results[i] = map_params_to_trajectory(input_dictionaries[i])
+            GC.gc()
+            CUDA.reclaim()
+            next!(progress)
+        end
+        return results
     end
 
-    return results
+    if execution_mode==:distributed
+        # Distributed
+        results = @showprogress pmap(input_dictionaries) do dict
+            map_params_to_trajectory(dict)
+        end
+        return results
+    end
+
+    if execution_mode==:serial
+        results = convert(Matrix{Any}, similar(input_dictionaries))
+        progress = Progress(length(results))
+        for i in 1:length(results)
+            results[i] = map_params_to_trajectory(input_dictionaries[i])
+            next!(progress)
+        end
+        return results
+    end
+
+    error("Execution mode is not implemented: $execution")
 end
 
 function save_results(path, results)
-    results = dict_to_cpu.(deepcopy(results))
+    results = deepcopy(results)
     # Make sure that the results are put on the CPU
     @save path results
 end
 
-function run_mnist_problem(; use_local_execution=false)
-    results = mnist_trajectory_experiment(;use_local_execution)
+function run_mnist_problem(; execution_mode=:serial)
+    results = mnist_trajectory_experiment(;execution_mode)
 
-    save_results("results/large/mnist_data.bson", results)
+    save_results("results/large/mnist_data_1.bson", results)
 end
