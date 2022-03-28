c7df3262bf4e1d6179f960c7a5a7c97d4767dcd3
Jamie Mair <jamie.mair@hotmail.co.uk>
2022-03-28 11:23:27 +0100

    Added device options to testing the accuracy

--- plotting/mnist_plotting.jl
+++ plotting/mnist_plotting.jl
@@ -29,22 +29,22 @@ function create_image_plots(digits...)
     return plt
 end
 
-function reconstruct_mnist_models(info_dict)
-    model = generate_mnist_model(;outputs=2)
+function reconstruct_mnist_models(info_dict; outputs=2, device=cpu)
+    model = generate_mnist_model(;outputs)
     _, re = Flux.destructure(model)
     if info_dict[:τ] == 1
         model = re(info_dict[:final_state]) # reconstruct model from params
-        return [model]
+        return [model |> device]
     end
 
-    models = [re(state) for state in info_dict[:final_state]]
+    models = [re(state)|>device for state in info_dict[:final_state]]
     return models
 end
 
-function measure_train_accuracy(info_dict)
-    models = reconstruct_mnist_models(info_dict)
-    features = info_dict[:features]
-    labels = info_dict[:labels]
-    accuracies = [mean(reshape((x->x[1]-1).(argmax(m(features), dims=1)), :) .== labels) for m in models]
+function measure_train_accuracy(info_dict; device=cpu, outputs=2)
+    models = reconstruct_mnist_models(info_dict; outputs, device)
+    features = info_dict[:features] |> device
+    labels = info_dict[:labels] |> cpu
+    accuracies = [Flux.mean(reshape((x->x[1]-1).(argmax(m(features) |> cpu, dims=1)), :) .== labels) for m in models]
     return accuracies
 end
