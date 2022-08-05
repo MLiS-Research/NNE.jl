e32d779eccf90e0b216aff6b81fe36bdc1de531b
Jamie Mair <JamieMair@users.noreply.github.com>
2022-08-05 09:20:08 +0000

    Changed MNIST script to run using the experiment interface

--- experiments/mnist.jl
+++ experiments/mnist.jl
@@ -1,68 +1,28 @@
-using Distributed
-@everywhere include("distributed/mnist_runner.jl")
-include("run_helper.jl")
-import Base.Iterators: product
-using BSON: @save, @load
-using ProgressMeter
-using Statistics
-using Flux
-using CUDA
-using TPS
-using NNE
-using NNE.Runner
-using TPS.Convergence
+using Revise
+using NNE.Experimenter
 
-function get_experiment_parameter_dictionaries(;device=:gpu, epochs=1_000_000)
-    s_min = 0.01
-    s_max = 5.0
-    num_s_values = 9
-    s_values = get_exponentially_spaced(s_min, s_max, num_s_values)
-    trajectory_lengths = [1, 2, 4, 8, 16]
-    options = Dict{Symbol, Any}()
-    options[:fraction_to_include] = 0.25
-    options[:epochs] = epochs
-    options[:device] = device
-    options[:n_samples] = 2048
-    options[:outputs] = 2
-    options[:σ] = 0.05
-    function construct_dict(s, τ)
-        new_options = deepcopy(options)
-        new_options[:s] = s
-        new_options[:τ] = τ
-        return new_options
-    end
-    input_dictionaries = [construct_dict(s, τ) for (s, τ) in product(s_values, trajectory_lengths)]
-    return input_dictionaries
-end
+config = Dict{Symbol,Any}(
+    :s => LogLinearVariable(0.01, 5.0, 9),
+    :τ => IterableVariable([1, 2, 4, 8, 16]),
+    :n_samples => 2048,
+    :outputs => 2,
+    :σ => 0.05,
+    :fraction_to_include => 0.25,
+    :device => :gpu,
+    :epochs => 100
+)
 
-function mnist_trajectory_experiment(; mode::TaskExecutionMode=SerialMode, device=:gpu, show_progress=false, kwargs...)
-    input_dictionaries = get_experiment_parameter_dictionaries(; device=device, kwargs...)
-    map_params_to_trajectory
-    return get_results(map_params_to_trajectory, input_dictionaries, mode; show_progress)
-end
+experiment = Experiment(
+    name="MNIST Experiment 1",
+    include_file="distributed/mnist_runner.jl",
+    function_name="map_params_to_trajectory",
+    configuration=config
+)
 
-function save_results(path, results)
-    results = deepcopy(results)
-    # Make sure that the results are put on the CPU
-    @save path results
-end
+db = open_db("experiments.db", joinpath(pwd(), "experiments", "results"))
 
-function load_results(path)
-    results = nothing
-    @load path results
-    return results
-end
+runner = Runner(execution_mode=SerialMode, experiment=experiment, database=db)
 
-function run_and_save_mnist_problem(; kwargs...)
-    results = mnist_trajectory_experiment(; kwargs...)
-    try
-        git_hash = get_git_hash()
-        for r in results
-            r[:git_hash] = git_hash
-        end
-    catch
-        println("Was not able to get Git hash.")
-    end
+execute(runner)
 
-    save_results("results/large/mnist_data.bson", results)
-end
+# trials = get_trials_by_name(db, "MNIST Experiment 1")
