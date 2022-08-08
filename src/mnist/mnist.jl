860e138915e0db08a3b925208f43d09c30bddbe0
Jamie Mair <JamieMair@users.noreply.github.com>
2022-08-08 10:41:32 +0000

    Created an experiment to compare efficiency of single model perturbations vs whole trajectory ones

--- src/mnist/mnist.jl
+++ src/mnist/mnist.jl
@@ -133,9 +133,9 @@ function solve_mnist_sa_automatic(; s=200.0, σ=0.001, n_samples=2048, device=cp
 end
 
 
-function solve_mnist_trajectory(; τ=4, s=50.0, σ=0.001, epochs=10000, n_samples=2048, device=cpu, outputs=2, show_progress=false, fraction_to_include=1.0, kwargs...)
+function solve_mnist_trajectory(; τ=4, s=50.0, σ=0.001, epochs=10000, n_samples=2048, device=cpu, outputs=2, show_progress=false, fraction_to_include=1.0, max_perturb_models::Int=nothing, kwargs...)
     problem, info = create_mnist_trajectory_state_and_loss(τ, σ, n_samples; device, outputs)
-    alg = MetropolisHastings.get_shooting_mh_alg(s, σ; fraction_to_include=fraction_to_include)
+    alg = MetropolisHastings.get_shooting_and_bridging_mh_alg(s, σ; fraction_to_include=fraction_to_include, max_width=max_perturb_models)
     iter = show_progress ? ProgressBar(1:epochs) : epochs
     solution = solve(problem, alg, iter)
     info[:solution] = solution
@@ -149,9 +149,9 @@ function solve_mnist_trajectory(; τ=4, s=50.0, σ=0.001, epochs=10000, n_sample
     return info
 end
 
-function solve_mnist_trajectory_automatic(; τ=4, s=500.0, σ=0.05, n_samples=2048, device=cpu, outputs=2, fraction_to_include=1.0, warmup_steps=0, polling_frequency=1, max_buffer_size=10000, relative_gradient_size=1e-4, relative_error_size=2e-2, max_epochs=100000)
+function solve_mnist_trajectory_automatic(; τ=4, s=500.0, σ=0.05, n_samples=2048, device=cpu, outputs=2, fraction_to_include=1.0, warmup_steps=0, polling_frequency=1, max_buffer_size=10000, relative_gradient_size=1e-4, relative_error_size=2e-2, max_epochs=100000, max_perturb_models::Int=nothing)
     problem, info = create_mnist_trajectory_state_and_loss(τ, σ, n_samples; device, outputs)
-    alg = MetropolisHastings.get_shooting_mh_alg(s, σ; fraction_to_include=fraction_to_include)
+    alg = MetropolisHastings.get_shooting_and_bridging_mh_alg(s, σ; fraction_to_include=fraction_to_include, max_width=max_perturb_models)
     iteration_options = AutomaticConvergenceOptions(warmup_steps, polling_frequency, max_buffer_size, max_epochs, relative_gradient_size, relative_error_size)
     sol = solve(problem, alg, iteration_options)
     info[:solution] = sol
