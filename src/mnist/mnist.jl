09cb31897623107cc2f95dbf6c5e4b0bed3ca596
Jamie Mair <JamieMair@users.noreply.github.com>
2022-08-09 19:45:17 +0000

    Upgraded to new TPS API

--- src/mnist/mnist.jl
+++ src/mnist/mnist.jl
@@ -3,8 +3,8 @@ module MNISTTraining
 using Flux
 using MLDatasets
 using TPS
-using TPS.SimulatedAnnealing
 using TPS.MetropolisHastings
+using TPS.SimulatedAnnealing
 using TPS.DiscreteTrajectory
 using TPS.Convergence
 using Random
@@ -44,10 +44,12 @@ function generate_mnist_dataset(n_samples::Int; seed::Int=659863, device=cpu, ou
 end
 
 function get_mnist_testing_dataset(; device=cpu, outputs=10)
-    labels = MNIST.testlabels()
+    dataset = MNIST(split=:test)
+    labels = dataset.targets
     selector = labels .< outputs
     labels = labels[selector] |> device
-    data = reshape(MNIST.testtensor(Float32)[:, :, selector], 28, 28, 1, :) |> device
+    features = dataset.features
+    data = reshape(features[:, :, selector], 28, 28, 1, :) |> device
     return data, labels
 end
 
@@ -72,10 +74,9 @@ function create_mnist_trajectory_state_and_loss(τ, σ, n_samples; rng=Random.GL
     _, info = create_mnist_sa_problem(n_samples; kwargs...)
     θ = info[:initial_state]
     loss_fn = info[:loss_fn]
-    states = []
-    push!(states, θ)
+    states = [θ]
     previous_state = θ
-    for t = 2:τ
+    for _ = 2:τ
         next_state = similar(θ)
         randn!(rng, next_state)
         next_state .= next_state .* σ .+ previous_state
@@ -102,7 +103,7 @@ end
 
 function solve_mnist_sa(; s=500.0, σ=0.001, epochs=100000, n_samples=4096, fraction_to_include=1.0, device=cpu, outputs=2, show_progress=false, kwargs...)
     problem, info = create_mnist_sa_problem(n_samples; device, outputs)
-    alg = gaussian_sa_algorithm(s, σ; params_changed_frac=fraction_to_include)
+    alg = TPS.MetropolisHastings.gaussian_sa_algorithm(s, σ; params_changed_frac=fraction_to_include)
     iter = show_progress ? ProgressBar(1:epochs) : epochs
     solution = solve(problem, alg, iter)
     info[:solution] = solution
@@ -118,7 +119,7 @@ end
 
 function solve_mnist_sa_automatic(; s=200.0, σ=0.001, n_samples=2048, device=cpu, outputs=2, fraction_to_include=1.0, warmup_steps=0, polling_frequency=1, max_buffer_size=10000, relative_gradient_size=1e-4, relative_error_size=2e-2, max_epochs=100000)
     problem, info = create_mnist_sa_problem(n_samples; device, outputs)
-    alg = gaussian_sa_algorithm(s, σ; params_changed_frac=fraction_to_include)
+    alg = TPS.MetropolisHastings.gaussian_sa_algorithm(s, σ; params_changed_frac=fraction_to_include)
     iteration_options = AutomaticConvergenceOptions(warmup_steps, polling_frequency, max_buffer_size, max_epochs, relative_gradient_size, relative_error_size)
     solution = solve(problem, alg, iteration_options)
     info[:solution] = solution
