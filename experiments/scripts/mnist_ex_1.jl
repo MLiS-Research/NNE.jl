cf95ecd75b78e156d29f5f91c2a5cfa4978d2b47
Jamie Mair <JamieMair@users.noreply.github.com>
2022-08-17 18:19:43 +0000

    Changed initial experiment to have a different range

--- experiments/scripts/mnist_ex_1.jl
+++ experiments/scripts/mnist_ex_1.jl
@@ -2,29 +2,29 @@ using NNE
 using NNE.Experimenter
 using NNE.Experimenter: @execute
 
-num_repeats = 15
+num_repeats = 1
 
 config = Dict{Symbol,Any}(
-    :s => LogLinearVariable(0.01, 5.0, 7),
+    :s => LogLinearVariable(5.0, 50.0, 5),
     :τ => IterableVariable([1, 2, 4, 8, 16]),
     :n_samples => 2048,
-    :outputs => 2,
+    :outputs => 10,
     :σ => 0.05,
     :fraction_to_include => 0.25,
     :device => :gpu,
-    :epochs => 300_000,
+    :epochs => 2_500_000,
     :dataset_seed => 46938723,
     :model_seed => 13124,
     :repeat_number => IterableVariable(1:num_repeats),
 )
 
 experiment = Experiment(
-    name="MNIST Experiment 200k",
+    name="MNIST Full 1",
     include_file="distributed/mnist_runner.jl",
     function_name="map_params_to_trajectory",
     configuration=config
 )
 
-db = open_db("experiments.db", joinpath(pwd(), "results", "large"))
+db = open_db("experiments_new.db", joinpath(pwd(), "results", "large"))
 
 @execute experiment db DistributedMode
