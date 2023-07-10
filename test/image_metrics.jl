3677506662b8043885f0203b86bf65fd9695c8b6
Jamie Mair <JamieMair@users.noreply.github.com>
2023-07-10 16:31:34 +0100

    Updated unit tests to use the new interfaces

--- test/image_metrics.jl
+++ test/image_metrics.jl
@@ -4,7 +4,6 @@ using TestItems
 @testitem "Test MNIST loss calculation on CPU" begin
     using NNE
     using NNE.ImageClassification
-    import NNE.Utils
     import Flux
 
     device = Flux.cpu
@@ -24,60 +23,52 @@ end
     gpu_model = generate_image_model(:MNIST; device, outputs=10)
     config = PreprocessConfig(shuffle=true, max_samples_per_label=16)
     gpu_dataset = load_dataset(:MNIST; split=SplitTest, device, config)
-    gpu_loss_fn = NNE.ImageClassification.ImageCrossEntropyLossObservable(gpu_model, gpu_dataset)
+    gpu_loss_fn = NNE.Utils.FluxCrossEntropyLossObservable(gpu_model, gpu_dataset)
 
     gpu_loss_single = gpu_loss_fn(gpu_model.parameters)
-    trajectory_length = 4
-    gpu_loss_trajectory = gpu_loss_fn([gpu_model.parameters for _ in 1:trajectory_length])
-
-    @test all(gpu_loss_trajectory .≈ [gpu_loss_single for _ in 1:trajectory_length])
 
     device = Flux.cpu
     model = generate_image_model(:MNIST; device, outputs=10)
     dataset = load_dataset(:MNIST; split=SplitTest, device, config)
-    loss_fn = NNE.ImageClassification.ImageCrossEntropyLossObservable(model, dataset)
+    loss_fn = NNE.Utils.FluxCrossEntropyLossObservable(model, dataset)
 
     loss_single = loss_fn(model.parameters)
 
     @test gpu_loss_single ≈ loss_single
 end
-
-@testitem "Test MNIST accuracy on CPU" begin
+@testitem "Test MNIST GPU predictions" begin
     using NNE
     using NNE.ImageClassification
     import Flux
-
-    device = Flux.cpu
-    model = generate_image_model(:MNIST; device, outputs=10)
-    config = PreprocessConfig(shuffle=true, max_samples_per_label=16)
-    dataset = load_dataset(:MNIST; split=SplitTest, device, config)
-    accuracy_fn = NNE.ImageClassification.generate_ensemble_accuracy_fn(dataset, model)
-
-    trajectory_length = 4
-    @test accuracy_fn([model.parameters for _ in 1:trajectory_length]) ≈ accuracy_fn(model.parameters)
-end
-@testitem "Test MNIST accuracy calculation on GPU" begin
-    using NNE
-    using NNE.ImageClassification
-    import Flux
+    import NNE.Interfaces: predict, features, labels
 
     device = Flux.gpu
     gpu_model = generate_image_model(:MNIST; device, outputs=10)
     config = PreprocessConfig(shuffle=true, max_samples_per_label=16)
     gpu_dataset = load_dataset(:MNIST; split=SplitTest, device, config)
-    gpu_accuracy_fn = NNE.ImageClassification.generate_ensemble_accuracy_fn(gpu_dataset, gpu_model)
 
-    gpu_accuracy_single = gpu_accuracy_fn(gpu_model.parameters)
-    trajectory_length = 4
-    gpu_accuracy_trajectory = gpu_accuracy_fn([gpu_model.parameters for _ in 1:trajectory_length])
-
-    @test gpu_accuracy_trajectory ≈ gpu_accuracy_single
+    gpu_predictions = NNE.Interfaces.predict(gpu_model, features(gpu_dataset))
 
     device = Flux.cpu
     model = generate_image_model(:MNIST; device, outputs=10)
     dataset = load_dataset(:MNIST; split=SplitTest, device, config)
-    accuracy_fn = NNE.ImageClassification.generate_ensemble_accuracy_fn(dataset, model)
 
-    @test accuracy_fn(model.parameters) ≈ gpu_accuracy_single
+    cpu_predictions = NNE.Interfaces.predict(model, features(dataset))
+
+    @test all(Array(gpu_predictions) .== cpu_predictions)
 end
+@testitem "Test MNIST accuracy on CPU" begin
+    using NNE
+    using NNE.ImageClassification
+    import Flux
+    import NNE.Interfaces: predict, features, labels
 
+    device = Flux.cpu
+    model = generate_image_model(:MNIST; device, outputs=10)
+    config = PreprocessConfig(shuffle=true, max_samples_per_label=16)
+    dataset = load_dataset(:MNIST; split=SplitTest, device, config)
+
+    predictions = NNE.Interfaces.predict(model, features(dataset))
+
+    @test typeof(NNE.Utils.accuracy(labels(dataset), predictions)) <: Any
+end
