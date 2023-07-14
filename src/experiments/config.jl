8af9fd7ffc73060ef096e1f86c0e2634931a4949
Jamie Mair <JamieMair@users.noreply.github.com>
2023-07-14 08:47:38 +0100

    Added interface to add tensorboard logging in the run function

--- src/experiments/config.jl
+++ src/experiments/config.jl
@@ -3,9 +3,17 @@ struct AlgorithmConfig{BT<:Union{Float32,Float64},ST<:Union{Float32,Float64}}
     sigma::ST
     parameter_perturb_fraction::Float64
 end
+struct TensorboardLoggingConfig
+    path::String
+    train_loss_frequency::Int
+    train_accuracy_frequency::Int
+    validation_loss_frequency::Int
+    validation_accuracy_frequency::Int
+end
 struct ExperimentConfig{AC<:AlgorithmConfig}
     trajectory_length::Int
     epochs::Int
     algorithm_config::AC
     use_progress::Bool
+    tensorboard_logging_config::Union{Nothing,TensorboardLoggingConfig}
 end
--- src/experiments/experiments.jl
+++ src/experiments/experiments.jl
@@ -6,6 +6,7 @@ import TransitionPathSampling as TPS
 using Logging
 using ProgressBars
 import CUDA: CuArray
+import ..Callbacks as CB
 include("config.jl")
 
 
@@ -54,8 +55,54 @@ function save_solution!(results, solution::TPS.SimpleSolution)
     results[:observations] = deepcopy(solution.observations)
 end
 
+function construct_callbacks(config::ExperimentConfig, model::Interfaces.AbstractClassificationModel, train_dataset::Interfaces.AbstractClassificationDataset, validation_dataset::Interfaces.AbstractClassificationDataset)
+    # Extend with saving callbacks
+    if isnothing(config.tensorboard_logging_config)
+        cb = construct_tb_callback(config, validation_dataset)
+        return cb
+    end
 
-function run(config::ExperimentConfig, model::Interfaces.AbstractClassificationModel, dataset::Interfaces.AbstractClassificationDataset)
+    return nothing
+end
+function _train_metrics(config::ExperimentConfig)
+    modifier = Float32(1.0 / config.trajectory_length)
+    tb_config::TensorboardLoggingConfig = config.tensorboard_logging_config
+    loss_metric = tb_config.train_loss_frequency == 0 ? nothing : CB.TrainingLossMetricGatherer(modifier, tb_config.train_loss_frequency)
+    accuracy_metric = tb_config.train_accuracy_frequency == 0 ? nothing : CB.TrainingAccuracyMetricGatherer(tb_config.train_accuracy_frequency)
+    return loss_metric, accuracy_metric
+end
+function _validation_metrics(config::TensorboardLoggingConfig, dataset)
+    loss_metric = config.validation_loss_frequency == 0 ? nothing : CB.ValidationLossMetricGatherer(dataset, config.validation_loss_frequency)
+    accuracy_metric = config.validation_accuracy_frequency == 0 ? nothing : CB.ValidationAccuracyMetricGatherer(dataset, config.validation_accuracy_frequency)
+    return loss_metric, accuracy_metric
+end
+function construct_tb_metrics(config::TensorboardLoggingConfig, ::Nothing)
+    loss_metric, accuracy_metric = _train_metrics(config::ExperimentConfig)
+    metrics = filter(!isnothing, (loss_metric, accuracy_metric))
+    return metrics
+end
+function construct_tb_metrics(config::TensorboardLoggingConfig, validation_dataset::Interfaces.AbstractClassificationDataset)
+    train_metrics = construct_tb_callback(config, nothing)
+    validation_metrics = filter(!isnothing, _validation_metrics(config.tensorboard_logging_config, validation_dataset))
+    return (train_metrics..., validation_metrics...)
+end
+function construct_tb_callback(config::ExperimentConfig, validation_dataset::Interfaces.AbstractClassificationDataset)
+    metrics = construct_tb_metrics(config, validation_dataset)
+    if length(metrics) == 0
+        return nothing
+    end
+
+    tb_config::TensorboardLoggingConfig = config.tensorboard_logging_config
+    cb = CB.TBLoggerCallback(tb_config.path, metrics)
+    return cb
+end
+
+
+function run(config::ExperimentConfig,
+    model::Interfaces.AbstractClassificationModel,
+    dataset::Interfaces.AbstractClassificationDataset;
+    validation_dataset::Union{Nothing,Interfaces.AbstractClassificationDataset}=nothing
+)
     results = Dict{Symbol,Any}()
     Utils.save_to!(results, config)
 
@@ -67,7 +114,10 @@ function run(config::ExperimentConfig, model::Interfaces.AbstractClassificationM
     if config.use_progress
         iter = ProgressBar(iter)
     end
-    solution = TPS.solve(problem, alg, iter)
+
+    cb = construct_callbacks(config, model, dataset, validation_dataset)
+
+    solution = TPS.solve(problem, alg, iter; cb=cb)
     save_solution!(results, solution)
 
     return results
